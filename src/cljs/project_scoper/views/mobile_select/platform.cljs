9b421a05984745d02b7f5eebc10af4de8d204ff0
beavelar <bavelar@msudenver.edu>
2018-06-20 18:47:27 -0600

    Created mobile-select-platform view and routes, rename files: - to _

---
+++ src/cljs/project_scoper/views/mobile_select/platform.cljs
@@ -0,0 +1,8 @@
+(ns project-scoper.views.mobile-select.platform
+  (:require [re-frame.core :as rf]
+            [project-scoper.components.bubble :refer [bubble]]
+            [project-scoper.components.controls :refer [controls]]
+            [project-scoper.subs :as subs]))
+
+(defn mobile-select-platform []
+  [:div "Hello world"])
