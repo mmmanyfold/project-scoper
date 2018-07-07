e02c6d382b4fd0fbb862194dc83cb14d500d15ac
David A. Viramontes <dviramontes@gmail.com>
2018-07-07 16:43:20 -0600

    update: css

--- src/cljs/project_scoper/views/pdf_preview.cljs
+++ src/cljs/project_scoper/views/pdf_preview.cljs
@@ -11,10 +11,11 @@
             (js/document.querySelector "#pdf")
             (clj->js {:onrendered
                       (fn [canvas]
-                        (let [pdf (js/jsPDF. #js {:format #js [8.5, 11] :unit "in"})
+                        (let [pdf (js/jsPDF. (clj->js {:format [8.5, 11]
+                                                       :unit   "in"}))
                               img (-> canvas (.toDataURL "image/png"))]
                           (-> pdf
-                              (.addImage img 0 0 10 0)
+                              (.addImage img 0 0 0 0)
                               (.save "test.pdf"))))})))]
     (fn []
       [:div.pdf-prev
@@ -22,33 +23,33 @@
        [:button.pv3.ph4.ma2 {:on-click #(handle-download)} "download"]
        [:button.pv3.ph4.ma2 "send to mmmanyfold"]
        [:a {:href "/#/"} [:button.pv3.ph4.ma2 "start over"]]
-       [:hr.mb0]
+       [:hr]
        [:div#pdf
-        [:h1.f1.fw5.mt0 "Project Scope - "
+        [:h1.f1.fw5 "Project Scope - "
          [:span.title.fw5 "Website"] [:span "*"]]
-        [:p.measure.f4-ns "Client Contact"]
-        [:p.measure.f4-ns.contact "Sandeep Salter"]
-        [:p.measure.f4-ns.contact "sandeep@pictureroom.shop"]
-        [:hr.ma2]
-        [:h2.f2.pv3.fw5.redish "*Build on an existing project"]
+        [:div.measure.f5-ns "Client Contact"]
+        [:div.measure.f5-ns.contact "Sandeep Salter"]
+        [:div.measure.f5-ns.contact "sandeep@pictureroom.shop"]
+        [:hr]
+        [:h2.f2.pv1.fw5.redish "*Build on an existing project"]
         [:ol.f3
-         [:li.mv4 [:div "PURPOSE"
+         [:li.mv3 [:div "PURPOSE"
                    [:ul
                     [:li [:span "Primary: "] [:span.redish "Online Shop"]]
                     [:li [:span "Secondary: "] [:span.redish "Business Shop"]]]]]
-         [:li.mv4 [:p "PLATFORM"]
-          [:p.redish "Lightspeed"]]
-         [:li.mv4 [:p "INTEGRATIONS"]
-          [:p.redish "Facebook Events"]]
-         [:li.mv4 [:p "USER ACCOUNTS?"]
-          [:p.redish "No"]]
-         [:li.mv4 [:p "VISUAL IDENTITY"]
-          [:p.redish "Client will provide: Logo, Colors, Fonts, Icons, Images, Site Map, Site Mockups"]]
-         [:li.mv4 [:p "TIMELINE"]
-          [:p "Soft Deadline: " [:span.redish "September 5, 2018"]]
-          [:p "Proposed Start: " [:span.redish "July 1, 2018"]]]
-         [:li.mv4 [:p "COMMENTS"]
-          [:p.redish "We would like the ability to change out Featured Artist content & Staff Picks on a regular basis"]]
+         [:li.mv3 [:div "PLATFORM"]
+          [:div.redish "Lightspeed"]]
+         [:li.mv3 [:div "INTEGRATIONS"]
+          [:div.redish "Facebook Events"]]
+         [:li.mv3 [:div "USER ACCOUNTS?"]
+          [:div.redish "No"]]
+         [:li.mv3 [:div "VISUAL IDENTITY"]
+          [:div.redish "Client will provide: Logo, Colors, Fonts, Icons, Images, Site Map, Site Mockups"]]
+         [:li.mv3 [:div "TIMELINE"]
+          [:div "Proposed Start: " [:span.redish "July 1, 2018"]]
+          [:div "Soft Deadline: " [:span.redish "September 5, 2018"]]]
+         [:li.mv3 [:div "COMMENTS"]
+          [:div.redish "We would like the ability to change out Featured Artist content & Staff Picks on a regular basis"]]
          [:hr.ma2]
          [:div
           [:div.tr.flex.items-center.w-100.justify-end
