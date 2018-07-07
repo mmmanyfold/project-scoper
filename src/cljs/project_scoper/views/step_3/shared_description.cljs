60650247ceedc912b8e37670725428f418cdf782
eemshi <oreneemshi@gmail.com>
2018-07-07 16:29:34 -0600

    add textarea character counter

--- src/cljs/project_scoper/views/step_3/shared_description.cljs
+++ src/cljs/project_scoper/views/step_3/shared_description.cljs
@@ -1,10 +1,17 @@
 (ns project-scoper.views.step-3.shared-description
   (:require [re-frame.core :as rf]
             [project-scoper.components.controls :refer [controls]]
-            [project-scoper.subs :as subs]))
+            [project-scoper.subs :as subs]
+            [reagent.core :as reagent]))
 
 (defn description-view []
-  [:div
-   [:h2 "Please describe:"]
-   [:textarea {:rows 7}]
-   [controls true true]])
+  (let [chars (reagent/atom 280)]
+    (fn []
+      [:div
+       [:h2 "Please describe:"]
+       [:textarea
+        {:rows 7
+         :maxLength 280
+         :on-change (fn [e] (reset! chars (- 280 (count (-> e .-target .-value)))))}]
+       [:p [:span @chars] " characters remaining"]
+       [controls true true]])))
--- src/cljs/project_scoper/views/step_5.cljs
+++ src/cljs/project_scoper/views/step_5.cljs
@@ -1,10 +1,17 @@
 (ns project-scoper.views.step-5
   (:require [re-frame.core :as rf]
             [project-scoper.components.controls :refer [controls]]
-            [project-scoper.subs :as subs]))
+            [project-scoper.subs :as subs]
+            [reagent.core :as reagent]))
 
 (defn step-5-view []
-  [:div
-   [:h2 "Additional comments?"]
-   [:textarea {:rows 7}]
-   [controls true true]])
+  (let [chars (reagent/atom 280)]
+    (fn []
+      [:div
+       [:h2 "Additional comments?"]
+       [:textarea
+        {:rows 7
+         :maxLength 280
+         :on-change (fn [e] (reset! chars (- 280 (count (-> e .-target .-value)))))}]
+       [:p [:span @chars] " characters remaining"]
+       [controls true true]])))
