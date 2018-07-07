9240f9b06f3ba2aed10e91c187b78000c5cce13a
David A. Viramontes <dviramontes@gmail.com>
2018-07-07 16:17:53 -0600

    add: copy, editorconfig, styles

--- src/cljs/project_scoper/views/pdf_preview.cljs
+++ src/cljs/project_scoper/views/pdf_preview.cljs
@@ -11,24 +11,49 @@
             (js/document.querySelector "#pdf")
             (clj->js {:onrendered
                       (fn [canvas]
-                        (let [pdf (js/jsPDF.)
+                        (let [pdf (js/jsPDF. #js {:format #js [8.5, 11] :unit "in"})
                               img (-> canvas (.toDataURL "image/png"))]
                           (-> pdf
-                              (.addImage img 0 10 0 0)
+                              (.addImage img 0 0 10 0)
                               (.save "test.pdf"))))})))]
-    [:div.pdf-prev
-     [:h2 "PREVIEW"]
-     [:button "send to mmmanyfold"]
-     [:button {:on-click #(handle-download)} "download"]
-     [:button "start over"]
-     [:hr]
-     [:div#pdf
-      [:h1 "Project Scope - {product or service}*"]
-      [:hr]
-      [:p"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed tempus urna et pharetra pharetra massa massa ultricies. Diam donec adipiscing tristique risus nec. Vel quam elementum pulvinar etiam non. Nunc aliquet bibendum enim facilisis. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Cras ornare arcu dui vivamus arcu. Congue nisi vitae suscipit tellus mauris a. Gravida dictum fusce ut placerat orci. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. Ac felis donec et odio pellentesque diam volutpat commodo sed. In ornare quam viverra orci sagittis. Commodo odio aenean sed adipiscing diam donec. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Ipsum a arcu cursus vitae. Duis ut diam quam nulla porttitor massa. Viverra orci sagittis eu volutpat odio facilisis mauris. Etiam erat velit scelerisque in dictum non consectetur."]
-      [:p"Cras fermentum odio eu feugiat pretium nibh ipsum. Sit amet volutpat consequat mauris. Feugiat pretium nibh ipsum consequat nisl vel. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Aliquet nec ullamcorper sit amet risus nullam eget. Cursus mattis molestie a iaculis. Massa tincidunt dui ut ornare lectus sit amet est placerat. Amet volutpat consequat mauris nunc congue nisi vitae suscipit. Gravida in fermentum et sollicitudin ac. Porta nibh venenatis cras sed felis. Sem et tortor consequat id. Massa sed elementum tempus egestas. Turpis egestas sed tempus urna et pharetra pharetra. Tristique magna sit amet purus gravida. At risus viverra adipiscing at in tellus integer feugiat scelerisque."]
-      [:p"Odio tempor orci dapibus ultrices in. A diam sollicitudin tempor id eu. Nunc faucibus a pellentesque sit amet porttitor. Quisque egestas diam in arcu cursus euismod. Sit amet risus nullam eget felis eget nunc lobortis mattis. Morbi tristique senectus et netus et malesuada fames ac turpis. Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu. Diam ut venenatis tellus in. Praesent elementum facilisis leo vel fringilla. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Bibendum arcu vitae elementum curabitur vitae nunc. Sapien eget mi proin sed. Imperdiet sed euismod nisi porta lorem. Magna ac placerat vestibulum lectus mauris ultrices eros in cursus."]
-      [:p"Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Quam lacus suspendisse faucibus interdum. Morbi tincidunt ornare massa eget egestas. Ac feugiat sed lectus vestibulum mattis ullamcorper. Quisque non tellus orci ac auctor augue mauris. Venenatis tellus in metus vulputate eu scelerisque felis. Viverra maecenas accumsan lacus vel. Pharetra diam sit amet nisl suscipit. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Aliquam eleifend mi in nulla posuere. Viverra accumsan in nisl nisi scelerisque. Imperdiet sed euismod nisi porta lorem. Elementum integer enim neque volutpat ac tincidunt vitae. Donec massa sapien faucibus et molestie ac. Varius vel pharetra vel turpis nunc. Feugiat nisl pretium fusce id velit ut tortor pretium viverra."]
-      [:p"Aliquam vestibulum morbi blandit cursus. Purus sit amet volutpat consequat mauris nunc congue nisi. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Ac feugiat sed lectus vestibulum. Et leo duis ut diam quam nulla porttitor massa. Sit amet tellus cras adipiscing enim eu turpis. Ultricies mi quis hendrerit dolor magna eget est lorem. Justo eget magna fermentum iaculis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Orci porta non pulvinar neque laoreet suspendisse interdum. Justo eget magna fermentum iaculis eu non diam. Id volutpat lacus laoreet non curabitur gravida arcu."]]]))
-
-
+    (fn []
+      [:div.pdf-prev
+       [:h4 "PREVIEW"]
+       [:button.pv3.ph4.ma2 {:on-click #(handle-download)} "download"]
+       [:button.pv3.ph4.ma2 "send to mmmanyfold"]
+       [:a {:href "/#/"} [:button.pv3.ph4.ma2 "start over"]]
+       [:hr.mb0]
+       [:div#pdf
+        [:h1.f1.fw5.mt0 "Project Scope - "
+         [:span.title.fw5 "Website"] [:span "*"]]
+        [:p.measure.f4-ns "Client Contact"]
+        [:p.measure.f4-ns.contact "Sandeep Salter"]
+        [:p.measure.f4-ns.contact "sandeep@pictureroom.shop"]
+        [:hr.ma2]
+        [:h2.f2.pv3.fw5.redish "*Build on an existing project"]
+        [:ol.f3
+         [:li.mv4 [:div "PURPOSE"
+                   [:ul
+                    [:li [:span "Primary: "] [:span.redish "Online Shop"]]
+                    [:li [:span "Secondary: "] [:span.redish "Business Shop"]]]]]
+         [:li.mv4 [:p "PLATFORM"]
+          [:p.redish "Lightspeed"]]
+         [:li.mv4 [:p "INTEGRATIONS"]
+          [:p.redish "Facebook Events"]]
+         [:li.mv4 [:p "USER ACCOUNTS?"]
+          [:p.redish "No"]]
+         [:li.mv4 [:p "VISUAL IDENTITY"]
+          [:p.redish "Client will provide: Logo, Colors, Fonts, Icons, Images, Site Map, Site Mockups"]]
+         [:li.mv4 [:p "TIMELINE"]
+          [:p "Soft Deadline: " [:span.redish "September 5, 2018"]]
+          [:p "Proposed Start: " [:span.redish "July 1, 2018"]]]
+         [:li.mv4 [:p "COMMENTS"]
+          [:p.redish "We would like the ability to change out Featured Artist content & Staff Picks on a regular basis"]]
+         [:hr.ma2]
+         [:div
+          [:div.tr.flex.items-center.w-100.justify-end
+           [:span
+            [:small "generated with"]
+            [:br]
+            [:small "project-scoper.mmmanyfold.com"]]
+           [:div [:img.logo {:src "img/logo-purple.png"}]]]]]]])))
