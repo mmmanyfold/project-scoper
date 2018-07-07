1cff36eb4ce02367b1037ef1dc705c7e0b000272
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-07-07 15:54:10 -0600

    Adds controls to timeline view

--- src/cljs/project_scoper/views/step_4.cljs
+++ src/cljs/project_scoper/views/step_4.cljs
@@ -4,7 +4,10 @@
             [re-frame.core :as rf]
             [reagent.core :as reagent]
             [project-scoper.components.bubble :refer [bubble]]
-            [project-scoper.components.controls :refer [controls]]))
+            [project-scoper.components.controls :refer [controls]]
+            [project-scoper.subs :as subs]))
+
+(def date-picked (reagent/atom false))
 
 (defn step-4-view []
   (reagent/create-class
@@ -17,15 +20,20 @@
                                             :setDefaultDate true})
              end-picker (js/Pikaday. #js {:field    (js/document.getElementById "end-datepicker")
                                           :format   "D MMM YYYY"
-                                          :minDate (js/Date.)})]))
+                                          :minDate (js/Date.)
+                                          :onSelect #(reset! date-picked true)})]))
      :reagent-render
      (fn []
        [:div
-        [:h2 "Proposed start date:"]
-        [:input#start-datepicker {:type "text"}]
-        [:h2 "Deadline"]
-        [:input#end-datepicker {:type "text"}]
-        [:div.bubbles-wrap
-         [bubble :green "hard deadline" :deadline-type]
-         [bubble :blue "soft deadline" :deadline-type]]])}))
+        (let [deadline-type @(rf/subscribe [::subs/deadline-type])]
+          [:h2 "Proposed start date:"]
+          [:input#start-datepicker {:type "text"}]
+          [:h2 "Deadline"]
+          [:input#end-datepicker {:type "text"}]
+          [:div.bubbles-wrap
+           [bubble :green "hard deadline" :deadline-type]
+           [bubble :blue "soft deadline" :deadline-type]]
+          (when (and date-picked
+                     (not (nil? deadline-type)))
+            [controls true true]))])}))
 
