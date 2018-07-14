eb29c7268197e24d09bdfa095ce489a98162a590
eemshi <oreneemshi@gmail.com>
2018-07-14 13:47:25 -0600

    add comments indicating conditional items in pdf

--- src/cljs/project_scoper/views/pdf_preview.cljs
+++ src/cljs/project_scoper/views/pdf_preview.cljs
@@ -33,27 +33,52 @@
         [:hr.mt3]
         [:ol.f3.pt1.pb1.list-items
          [:li.mv3 [:div "Context: "
+                   ;; OR "Starting from scratch"
                    [:span.specs "Building on an existing project"]]]
+
+         ;; ONLY WEBSITE
          [:li.mv3 [:div "Website Purpose"]
                   [:ul
                    [:li [:span "Primary:"] [:span.specs "Online Shop"]]
+                   ;; IF NIL: "N/A"
                    [:li [:span "Secondary:"] [:span.specs "Business Shop"]]]]
+
+         ;; ONLY WEBSITE
          [:li.mv3 [:div "Platform: "
+                   ;; IF NIL: "Open to any"
                    [:span.specs "Lightspeed"]]]
+
+         ;; ONLY MOBILE APP
+         ;[:li.mv3 [:div "Platform: "
+         ;          [:span.specs "iOS & Android"]]]
+
+         ;; ONLY WEBSITE & MOBILE APP
          [:li.mv3 [:div "Integrations"]
                   [:ul
+                   ;; IF NIL: "None"
                    [:li [:span.specs "Facebook Events, Google Maps"]]]]
          [:li.mv3 [:div "Need user accounts? "
+                   ;; IF TRUE: "Yes"
                    [:span.specs "No"]]]
          [:li.mv3 [:div "Visual Identity"]
                   [:ul
+                   ;; IF NO: "Client needs branding services."
                    [:li [:span.specs "Client will provide Logo, Colors, Fonts, Icons, Images, Site Map, Site Mockups"]]]]
+
+         ;; ONLY SERVICE, INSTALLATION, & NOT SURE
+         ;[:li.mv3 [:div "Description"]
+         ; [:p.specs "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat."]]
+
+         ;; FOR ALL
          [:li.mv3 [:div "Timeline"]
                   [:ul
                    [:li [:span "Proposed Start:" [:span.specs "July 1, 2018"]]]
                    [:li [:span "Deadline:" [:span.specs "September 5, 2018 (soft)"]]]]]
+
+         ;; ONLY IF NOT NIL
          [:li.mv3 [:div "Comments"]
                   [:p.specs "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat."]]]
+
         [:hr.mt4.mb3]
         [:div.footer.tr.pt2.flex.items-center.w-100.justify-end
          [:p "generated with"
